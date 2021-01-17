662f51030763192cc726f34153f5af696ab1584f
shokir <shokir100@gmail.com>
2021-01-17 16:02:49 +0300

    Added DataBinding:Views

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -4,34 +4,39 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputBinding
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        val doneButton = findViewById<Button>(R.id.btnDone)
-        doneButton.setOnClickListener {
-            addNickname(it)  // it in this context refers to the Done button.
+//        val doneButton = findViewById<Button>(R.id.btnDone)
+//        doneButton.setOnClickListener {
+//            addNickname(it)  // it in this context refers to the Done button.
+//        }
+        binding.btnDone.setOnClickListener {
+            addNickname(it)
         }
     }
 
     private fun addNickname(view: View){
-        // parameter View is the view in which this function is being called. In our case, Button view.
-        val editText = findViewById<EditText>(R.id.etNicknameEdit)
-        val nicknameText = findViewById<TextView>(R.id.tvNicknameText)
-
-        nicknameText.text = editText.text
-        editText.visibility = View.GONE  // Hiding EditText
-        view.visibility = View.GONE      // Hiding Button
-        nicknameText.visibility = View.VISIBLE  // Make the TextView visible.
-
-        // Hide the keyboard when we are done.
-        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(view.windowToken,0)
+        binding.apply {
+            tvNicknameText.text = binding.etNicknameEdit.text
+            invalidateAll()  // refresh the UI with the new data.
+            etNicknameEdit.visibility = View.GONE
+            btnDone.visibility = View.GONE
+            tvNicknameText.visibility = View.VISIBLE
+        }
     }
 }
